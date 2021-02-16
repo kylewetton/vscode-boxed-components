7858be475a0cddef8b99ebda38f748b94f2049eb
Kyle Wetton <kyle.wetton@colensobbdo.co.nz>
2021-02-17 12:33:55 +1300

    comments

--- src/extension.js
+++ src/extension.js
@@ -5,78 +5,98 @@ const fs = require('fs');
 const utils = require('./utils');
 const path = require('path');
 
-const createChosenBox = async (config, template, projectRoot) => {
-	/**
-	 * TODO: This appears to only point to the first folder in the workspace.
-	 * Personally never really have more than one project open in one workspace at a time
-	 * */ 
-	const destURI = `${projectRoot}/${config[template]['dest']}`;
-	const srcURI = `${projectRoot}/${config[template]['src']}`;
-
-	const componentName = await vscode.window.showInputBox({
-		placeHolder: 'Name your component...',
-		validateInput: text => {
-
-			if (!text) {
-				return `You have to give it a name 😂`;
-			}
-
-			if (fs.existsSync(`${destURI}/${text}`)) {
-				return `${text} already exists? 😐`;
-			}
-
-			return null;
-		}
-	});
-
-	if (config)
-	{
-
-		if (!componentName)
-		{
-			vscode.window.showErrorMessage(`Something went wrong here, the component name you entered didn't reach me.`);
-			return false;
-		}
-
-		utils.copyDir(srcURI,`${destURI}/${componentName}`,componentName);
-		vscode.window.showInformationMessage(`Created ${componentName} ${template} successfully! 👋`);
-	}
-	else {
-	}
-}
-
-const quickPickTemplate = (templates, projectRoot) => {
-
-	if (!templates) {
-		vscode.window.showErrorMessage(`Couldn't find any templates in settings.json`);
-		return false;
-	}
-	
-	const quickPick = vscode.window.createQuickPick();
-		quickPick.placeholder = 'Select the template to copy from...';
-		quickPick.items = Object.keys(templates).map(label => ({ label }));
-		quickPick.onDidChangeSelection(selection => {
-			if (selection[0]) {
-				createChosenBox(templates, selection[0].label, projectRoot);
-			}
-		});
-		quickPick.onDidHide(() => quickPick.dispose());
-		quickPick.show();
-}
-
-const quickPickWorkspace =  async () => {
-	const folder = await vscode.window.showWorkspaceFolderPick({ placeHolder: 'Warning: Multi-root support has known bugs...' });
-	if (folder) {
-		const configuration = vscode.workspace.getConfiguration('', folder.uri);
-		const templates = configuration.get('boxed-components.useTemplates');
-		quickPickTemplate(templates, folder.uri.fsPath);
-	}
-}
-
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+
+	/**
+	 * createChosenBox is called after the user goes through quickPickTemplate
+	 * 
+	 * @param {string[]} config – An array of possible templates
+	 * @param {string} template – The template selected
+	 * @param {string} projectRoot – The project root defined by quickPickWorkspace() || workspaceFolders[0]
+	 */
+
+	const createChosenBox = async (config, template, projectRoot) => {
+	
+		const destURI = `${projectRoot}/${config[template]['dest']}`;
+		const srcURI = `${projectRoot}/${config[template]['src']}`;
+	
+		const componentName = await vscode.window.showInputBox({
+			placeHolder: 'Name your component...',
+			validateInput: text => {
+	
+				if (!text) {
+					return `You have to give it a name 😂`;
+				}
+	
+				if (fs.existsSync(`${destURI}/${text}`)) {
+					return `${text} already exists? 😐`;
+				}
+	
+				return null;
+			}
+		});
+	
+		if (config)
+		{
+	
+			if (!componentName)
+			{
+				vscode.window.showErrorMessage(`Something went wrong here, the component name you entered didn't reach me.`);
+				return false;
+			}
+	
+			utils.copyDir(srcURI,`${destURI}/${componentName}`,componentName);
+			vscode.window.showInformationMessage(`Created ${componentName} ${template} successfully! 👋`);
+		}
+		else {
+		}
+	}
+	
+	/**
+	 * If there is only one workspace, this is called immediately, else
+	 * its called after quickPickWorkspace()
+	 * 
+	 * @param {string[]} templates – An array of possible templates
+	 * @param {string} projectRoot – The project root defined by quickPickWorkspace() || workspaceFolders[0]
+	 */
+	const quickPickTemplate = (templates, projectRoot) => {
+	
+		if (!templates) {
+			vscode.window.showErrorMessage(`Couldn't find any templates in settings.json`);
+			return false;
+		}
+		
+		const quickPick = vscode.window.createQuickPick();
+			quickPick.placeholder = 'Select the template to copy from...';
+			quickPick.items = Object.keys(templates).map(label => ({ label }));
+			quickPick.onDidChangeSelection(selection => {
+				if (selection[0]) {
+					createChosenBox(templates, selection[0].label, projectRoot);
+				}
+			});
+			quickPick.onDidHide(() => quickPick.dispose());
+			quickPick.show();
+	}
+
+	/**
+	 * If there's more than one workspace, the user first selects which one to run this extension in
+	 */
+	
+	const quickPickWorkspace =  async () => {
+		const folder = await vscode.window.showWorkspaceFolderPick({ placeHolder: 'Warning: Multi-root support has known bugs...' });
+		if (folder) {
+			const configuration = vscode.workspace.getConfiguration('', folder.uri);
+			const templates = configuration.get('boxed-components.useTemplates');
+			quickPickTemplate(templates, folder.uri.fsPath);
+		}
+	}
+
+	/**
+	 * Register the createBox command
+	 */
 
 	const createBox = vscode.commands.registerCommand('boxed-components.createBox', () => {
 		
